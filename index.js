f4ab5e578892a2633d0d535f52e7709faf7b1ddb
Kelly Selden <kellyselden@gmail.com>
2019-05-06 15:26:41 +0100

    add eslint-plugin-json-files pinUnstable

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = {
   extends: 'eslint:recommended',
   plugins: [
+    'json-files',
     'prefer-let'
   ],
   rules: {
+    'json-files/restrict-ranges': [2, { pinUnstable: true }],
+
     'prefer-let/prefer-let': 2,
 
     // possible errors
